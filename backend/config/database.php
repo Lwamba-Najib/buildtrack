fab20ceb1e28e0da9300ca17edf0f0c90c00d672
ALPHAe <najib2019reloaded@gmail.com>
2026-09-13 09:40:58 +0300

    Force clean rebuild and verify pgsql config

--- backend/config/database.php
+++ backend/config/database.php
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require', // Changed from 'prefer' to 'require' for Render
+            'sslmode' => 'require', 
         ],
         
         'sqlsrv' => [
