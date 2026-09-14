e7f7c07f244b8d2327d4757375da65a3fb80cd3d
ALPHAe <najib2019reloaded@gmail.com>
2026-09-14 13:58:08 +0300

    Fix database.php pgsql configuration

--- backend/config/database.php
+++ backend/config/database.php
@@ -3,11 +3,9 @@
 use Illuminate\Support\Str;
 
 return [
-
     'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
@@ -63,20 +61,16 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-
     ],
 
     'migrations' => 'migrations',
 
     'redis' => [
-
         'client' => env('REDIS_CLIENT', 'phpredis'),
-
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
-
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -85,7 +79,6 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
-
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -94,7 +87,5 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
-
     ],
-
 ];
